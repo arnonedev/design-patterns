5a00fd17720ebc2ba933e8e0b12b85f3c7100ffd
Arkadiusz Suchenia <arek@arnonedev.pl>
2017-04-06 20:05:00 +0200

    Implemented startup code.

--- src/pl/arnonedev/strategy/Startup.java
+++ src/pl/arnonedev/strategy/Startup.java
@@ -1,7 +1,21 @@
 package pl.arnonedev.strategy;
 
+import pl.arnonedev.strategy.attacks.Arson;
+import pl.arnonedev.strategy.attacks.Drowning;
+import pl.arnonedev.strategy.cards.Card;
+import pl.arnonedev.strategy.cards.Catapult;
+
 /**
  * Created by Arek on 2017-04-05.
  */
 public class Startup {
+    public static void main(String[] args) {
+        Card catapult = new Catapult();
+        catapult.attack();
+        catapult.makeSpecialAttack();
+        catapult.setSpecialAttack(new Arson());
+        catapult.makeSpecialAttack();
+        catapult.setSpecialAttack(new Drowning());
+        catapult.makeSpecialAttack();
+    }
 }
