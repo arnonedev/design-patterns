9f0d7363d95c3dcfc998414c332cbce03d2a395d
Arkadiusz Suchenia <arek@arnonedev.pl>
2017-04-20 22:23:02 +0200

    Added startup implementation.

--- src/pl/arnonedev/decorator/Startup.java
+++ src/pl/arnonedev/decorator/Startup.java
@@ -1,7 +1,27 @@
 package pl.arnonedev.decorator;
 
+import pl.arnonedev.decorator.car.Car;
+import pl.arnonedev.decorator.car.Corvette;
+import pl.arnonedev.decorator.car.Mustang;
+import pl.arnonedev.decorator.decorator.Audio;
+import pl.arnonedev.decorator.decorator.Seats;
+import pl.arnonedev.decorator.decorator.Wheels;
+
 /**
  * Created by Arek on 2017-04-20.
  */
 public class Startup {
+    public static void main(String[] args) {
+        Car corvette = new Corvette();
+        System.out.println(corvette.price() + " " + corvette.getDescription());
+        corvette = new Wheels(corvette);
+        corvette = new Audio(corvette);
+        System.out.println(corvette.price() + " " + corvette.getDescription());
+
+        Car mustang = new Mustang();
+        mustang = new Seats(mustang);
+        mustang = new Audio(mustang);
+        mustang = new Wheels(mustang);
+        System.out.println("\n" + mustang.price() + " " + mustang.getDescription());
+    }
 }
