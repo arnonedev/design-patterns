19104d95572f7d6618bedfb18b4d0966f1b7c851
Arkadiusz Suchenia <arek@arnonedev.pl>
2017-05-07 16:54:05 +0200

    Changed real cars implementation.

--- src/pl/arnonedev/factory/method/model/A4.java
+++ src/pl/arnonedev/factory/method/model/A4.java
@@ -4,23 +4,11 @@ package pl.arnonedev.factory.method.model;
  * Created by Arek on 2017-05-07.
  */
 public class A4 extends Car {
-    @Override
-    public void setMake(String make) {
-        super.setMake("Audi");
-    }
 
-    @Override
-    public void setModel(String model) {
-        super.setModel("A4");
-    }
-
-    @Override
-    public void setVin(String vin) {
-        super.setVin("AUDI" + vin);
-    }
-
-    @Override
-    public void setYear(int year) {
-        super.setYear(year);
+    public A4() {
+        setMake("Audi");
+        setModel("A4");
+        setVin("AUDI");
+        setYear(2016);
     }
 }
--- src/pl/arnonedev/factory/method/model/Accord.java
+++ src/pl/arnonedev/factory/method/model/Accord.java
@@ -4,23 +4,11 @@ package pl.arnonedev.factory.method.model;
  * Created by Arek on 2017-05-07.
  */
 public class Accord extends Car {
-    @Override
-    public void setMake(String make) {
-        super.setMake("Honda");
-    }
 
-    @Override
-    public void setModel(String model) {
-        super.setModel("Accord");
-    }
-
-    @Override
-    public void setVin(String vin) {
-        super.setVin("HONDA" + vin);
-    }
-
-    @Override
-    public void setYear(int year) {
-        super.setYear(year);
+    public Accord() {
+        setMake("Honda");
+        setModel("Accord");
+        setVin("HONDA");
+        setYear(2016);
     }
 }
